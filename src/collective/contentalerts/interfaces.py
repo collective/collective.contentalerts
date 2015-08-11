a04e22e29de592c52c763b8024d7e95c3d319ffe
Gil Forcada <gforcada@gnome.org>
2015-08-11 14:01:10 +0200

    Add registry record to keep stop words

--- src/collective/contentalerts/interfaces.py
+++ src/collective/contentalerts/interfaces.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
-
+from collective.contentalerts import _
+from zope import schema
+from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class ICollectiveContentalertsLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IStopWords(Interface):
+
+    stop_words = schema.Text(
+        title=_(
+            u'settings_stop_words_list_title',
+            default=u'List'
+        ),
+        description=_(
+            u'settings_stop_words_list_description',
+            default=u'Words/sentences that will generate an alert, '
+                    u'one per line.'
+        ),
+        required=False,
+    )
---
+++ src/collective/contentalerts/tests/test_registry.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from collective.contentalerts.testing import COLLECTIVE_CONTENTALERTS_INTEGRATION_TESTING  # noqa
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+
+import unittest
+
+
+class RegistryTest(unittest.TestCase):
+    layer = COLLECTIVE_CONTENTALERTS_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_registry_record(self):
+        """Check that the registry record exists."""
+        registry = getUtility(IRegistry)
+        self.assertIn(
+            'collective.contentalerts.interfaces.IStopWords.stop_words',
+            registry
+        )
