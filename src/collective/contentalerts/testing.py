e8bd9b75751d85c0f60811819b43eb82b780c3c4
Gil Forcada <gforcada@gnome.org>
2015-08-11 14:09:32 +0200

    Sort imports as per p.r.codeanalysis

--- src/collective/contentalerts/testing.py
+++ src/collective/contentalerts/testing.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
-from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
 from plone.testing import z2
 
 import collective.contentalerts
