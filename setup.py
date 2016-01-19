f09f0140dafa702de4bb8832bbff4e4e1e365421
Gil Forcada <gforcada@gnome.org>
2016-01-19 17:24:21 +0100

    Back to development: 0.6
    [ci skip]

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = (
 
 setup(
     name='collective.contentalerts',
-    version='0.5',
+    version='0.6.dev0',
     description="An add-on for Plone to get alerts about content",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
