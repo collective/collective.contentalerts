d8cbefdff6071c89c2578ea4c0ee00615f53e678
Gil Forcada <gforcada@gnome.org>
2016-01-22 09:19:38 +0100

    Async browser view to review objects
    If collective.taskqueue is available,
    queue a view to handle object verification for the new stop words added.
    
    This way adding a new stop word returns quickly and the review process
    is processed asynchronously and batched.

--- src/collective/contentalerts/handlers.py
+++ src/collective/contentalerts/handlers.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
+from collective.contentalerts import ASYNC
+from collective.contentalerts import logger
 from collective.contentalerts.interfaces import IStopWordsVerified
 from collective.contentalerts.utilities import get_new_entries
 from collective.contentalerts.utilities import verify_brain
 from plone import api
+
+
+if ASYNC:
+    from collective.taskqueue import taskqueue
 
 
 def review_verified_objects(settings, event):
@@ -18,5 +24,29 @@ def review_verified_objects(settings, event):
     catalog = api.portal.get_tool('portal_catalog')
     brains = catalog(object_provides=IStopWordsVerified.__identifier__)
 
-    for brain in brains:
-        verify_brain(brain, new_entries)
+    if not ASYNC:
+        for brain in brains:
+            verify_brain(brain, new_entries)
+    else:
+        # split the work to review the verified objects for new stop words in
+        # batches
+        batch = 1
+        amount = 300
+        count = len(brains)
+        while count > 0:
+            view_path = '/{0}/@@review-objects?'.format(
+                api.portal.get().id
+            )
+            params = {
+                'start': amount * batch - amount,
+                'size': amount,
+                'entries': new_entries,
+            }
+            logger.warn('Queued request {0} {1}'.format(view_path, params))
+            taskqueue.add(
+                view_path,
+                params=params
+            )
+
+            batch += 1
+            count -= amount
